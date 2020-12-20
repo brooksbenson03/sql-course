a66176f9c15419232c12d51189c14a3b8d011872
Brooks Benson <brooksbenson03@gmail.com>
2020-12-19 17:10:07 -0700

    ctes

--- instagram-clone/reference/common-table-expressions.sql
+++ instagram-clone/reference/common-table-expressions.sql
@@ -1,50 +1,27 @@
--- no cte
-
-SELECT username, tags.created_at
-FROM users
-JOIN (
-  SELECT user_id, created_at FROM caption_tags
-  UNION ALL
-  SELECT user_id, created_at FROM photo_tags
-) AS tags ON tags.user_id = users.id
-WHERE tags.created_at > '2010-01-07';
-
--- with cte
--- defined by the WITH keyword
+-- simple cte
+-- makes things more readable with no performace impact
 
 WITH tags AS (
-  SELECT user_id, created_at FROM caption_tags
-  UNION ALL
-  SELECT user_id, created_at FROM photo_tags
+    SELECT user_id, created_at FROM caption_tags
+    UNION ALL
+	SELECT user_id, created_at FROM photo_tags
+)
+ 
+SELECT users.username, tags.created_at
+FROM users
+JOIN tage ON tags.user_id = users.id
+WHERE tags.created_at < '2010-01-07';
+
+-- recursive cte
+-- nice when working with a tree or graph as data structure
+-- every recursive cte will include a union
+
+WITH RECURSIVE countdown(val) AS (
+  SELECT 3 as val
+  UNION
+  SELECT val - 1
+  FROM countdown
+  WHERE val > 1
 )
 
-SELECT username, tags.created_at
-FROM users
-JOIN tags ON tags.user_id = users.id
-WHERE tags.created_at > '2010-01-07';
-
--- two forms of cte:
-
--- 1) for readability (above is for readability)
--- 2) for recursion
-
-WITH RECURSIVE countdown(val) AS ( -- 'countdown' is the name of cte
-  SELECT 3 AS val -- initial, non-recursive
-  UNION
-  SELECT val - 1 FROM countdown WHERE var > 1 -- recursive
-);
-
--- recursive is good from graphs or trees
-
--- following query is for follow suggestions
-
-WITH RECURSIVE suggestions(leader_id, follower_id, depth) AS (
-  SELECT leader_id, follower_id, 1 AS depth
-  FROM followers
-  WHERE follower_id = 1000
-  UNION
-  SELECT followers.leader_id, followers.follower_id, depth + 1
-  FROM followers
-  JOIN suggestions ON suggestions.leader_id = followers.follower_id
-  WHERE depth < 3
-);
+SELECT * FROM countdown
